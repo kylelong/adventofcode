4c8772d791adfabf8406180558da484faa1aed1d
Kyle Long <kylel95@vt.edu>
2024-12-03 23:24:32 -0500

    aoc day3 pt.1

---
+++ 2024/day3/one.py
@@ -0,0 +1,12 @@
+import re
+with open("input.txt", "r") as file:
+    res = 0
+    for line in file:
+        matches = re.findall(r"mul\(\d+,\s*\d+\)", line)
+        for m in matches:
+            m = m.replace('mul(', ' ')
+            m = m.replace(')', ' ')
+            numbers = m.split(',')
+            a, b = int(numbers[0]), int(numbers[1])
+            res += a * b
+    print(res)
